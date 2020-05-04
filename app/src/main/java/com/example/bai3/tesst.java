a0fce3fe2be841ee51cbcce010ce8b9a80cfe775
nghiachaunamdinh <nghiachaunghiahung.com>
2020-05-04 22:12:52 +0700

    commit

---
+++ app/src/main/java/com/example/bai3/tesst.java
@@ -0,0 +1,14 @@
+package com.example.bai3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tesst extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tesst);
+    }
+}
